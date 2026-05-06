d6c5c7c07e55e68bf9d26a5f6f69ec54224ab1dd
pantamitsd <amitpantdataanalyst@gmail.com>
2026-05-06 12:55:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pytesseract
 from pdf2image import convert_from_bytes
 
 # -------- TESSERACT PATH --------
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # -------- PAGE TITLE --------
 st.title("📄 PDF Invoice Extractor For Swiss Military")
